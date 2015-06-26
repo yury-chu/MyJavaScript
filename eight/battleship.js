bc6afdc837c3d5a8a59f9e787a092bc2f219d100
yury_chu <tesewar@mail.ru>
2015-06-26 12:03:36 +0400

    release battleship

--- eight/battleship.js
+++ eight/battleship.js
@@ -21,13 +21,13 @@ var model = {
 	shipLength: 3,
 	shipsSunk: 0,
 	ships: [{
-		locations:["06","16","26"],
+		locations:["0","0","0"],
 		hits: ["","",""]
 	},{
-		locations:["24","34","44"],
+		locations:["0","0","0"],
 		hits: ["","",""]
 	},{
-		locations:["10","11","12"],
+		locations:["0","0","0"],
 		hits: ["","",""]
 	}],
 	fire: function(guess) {
@@ -56,6 +56,46 @@ var model = {
 			}
 		}
 		return true;
+	},
+	generateShipLocations: function() {
+		var locations;
+		for (var i = 0; i < this.numShips; i++) {
+			do {
+				locations = this.generateShip();
+			} while (this.collision(locations));
+			this.ships[i].locations = locations;
+		}
+	},
+	generateShip: function() {
+		var direction = Math.floor(Math.random() * 2);
+		var row, col;
+		if (direction === 1) {
+			row = Math.floor(Math.random() * this.boardSize);
+			col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+		} else {
+			row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+			col = Math.floor(Math.random() * this.boardSize);
+		}
+		var newShipLocations = [];
+		for (var i = 0; i < this.shipLength; i++) {
+			if (direction === 1) {
+				newShipLocations.push(row + "" + (col + i));
+			} else {
+				newShipLocations.push((row + i) + "" + col);
+			}
+		}
+		return newShipLocations;
+	},
+	collision: function(locations) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			for (var j = 0; j < locations.length; j++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 };
 
@@ -99,6 +139,8 @@ function init() {
 	fireButton.onclick = handleFireButton;
 	var guessInput = document.getElementById("guessInput");
 	guessInput.onkeypress = handleKeyPress;
+	
+	model.generateShipLocations();
 }
 
 function handleFireButton() {
